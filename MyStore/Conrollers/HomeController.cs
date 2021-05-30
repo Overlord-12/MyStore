04913c8b00243cfc630c9705b33e9953afaf4768
Overlord-12 <danya.argastesv.02@mail.ru>
2021-05-30 14:31:31 +0300

    Добавление котроллеров

---
+++ MyStore/Conrollers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyStore.Conrollers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _Configuration;
+        public HomeController (IConfiguration configuration) { _Configuration = configuration; }
+        public IActionResult Index()
+        {
+            return Content("Hello from my Control");
+        }
+        public IActionResult SecondAction()
+        {
+            return Content(_Configuration["Greeting"]);
+        }
+    }
+}
--- MyStore/Startup.cs
+++ MyStore/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,10 +13,17 @@ namespace MyStore
 {
     public class Startup
     {
+
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,12 +36,14 @@ namespace MyStore
 
             app.UseRouting();
 
+            var greetings = Configuration["Greeting"];
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
+                endpoints.MapGet("/greeting", async context =>
                 {
-                    await context.Response.WriteAsync("ÑÀËÀÌ ÂÑÅ ËÞÄßÌ");
+                    await context.Response.WriteAsync(greetings);
                 });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
